a838f29ee0021ed65c20dd9f3e5bdb9a4836c242
Frank Parejo <frank.parejo@sympower.net>
2025-02-28 23:19:34 +0100

    add infinite scroll to the repository list

--- client/src/components/RepositoryList.tsx
+++ client/src/components/RepositoryList.tsx
@@ -1,6 +1,6 @@
 import { subscribeKeys } from "nanostores";
 import { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import { $repositoriesParamsStore, initialState } from "../stores/repositories";
 import { getGithubRepositories } from "../requests/repositories";
@@ -8,27 +8,38 @@ import styles from "./RepositoryList.module.css";
 import type { GetGithubRepositoriesParams } from "../models/repositories";
 import { queryClient } from "../stores/queryClient";
 import RepositoryCard from "./RepositoryCard";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function RepositoryList() {
   const [repositoriesParams, setRepositoriesParams] =
     useState<GetGithubRepositoriesParams>(initialState);
-  const { data, isLoading } = useQuery(
+  const { data, isPending, fetchNextPage } = useInfiniteQuery(
     {
+      initialPageParam: 1,
       queryKey: ["GET_REPOSITORY_LIST", repositoriesParams],
-      queryFn: () =>
-        getGithubRepositories({
+      queryFn: ({ pageParam }) => {
+        return getGithubRepositories({
           language: repositoriesParams.filters.language,
-          page: repositoriesParams.pagination.page,
+          page: pageParam,
           perPage: repositoriesParams.pagination.perPage,
-        }),
+        });
+      },
+      getNextPageParam(_, pages) {
+        const pagesLength = pages.length;
+
+        return pagesLength + 1;
+      },
     },
     queryClient,
   );
 
+  const repositories = data?.pages.flatMap((page) => page.repositories) ?? [];
+  const totalCount = data?.pages[0].totalCount ?? 0;
+
   useEffect(() => {
     const unsubscribe = subscribeKeys(
       $repositoriesParamsStore,
-      ["filters.language"],
+      ["filters.language", "pagination.page"],
       (storeParams) => {
         setRepositoriesParams(storeParams);
       },
@@ -37,7 +48,7 @@ function RepositoryList() {
     return () => unsubscribe();
   }, []);
 
-  if (isLoading) {
+  if (isPending) {
     return (
       <section className={styles.container}>
         <p>Loading...</p>
@@ -47,9 +58,18 @@ function RepositoryList() {
 
   return (
     <section className={styles.container}>
-      {data?.repositories.map((repository) => (
-        <RepositoryCard key={repository.id} repository={repository} />
-      ))}
+      <InfiniteScroll
+        dataLength={repositories.length ?? 0}
+        next={fetchNextPage}
+        hasMore={repositories.length < totalCount}
+        scrollThreshold={0.7}
+        loader={<h4>Loading...</h4>}
+        className={styles.infiniteScrollContainer}
+      >
+        {repositories.map((repository) => (
+          <RepositoryCard key={repository.id} repository={repository} />
+        ))}
+      </InfiniteScroll>
     </section>
   );
 }
