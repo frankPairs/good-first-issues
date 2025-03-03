87e4b19dbf0bc16c01ef2f3a7430661fda179aef
Frank Parejo <frank.parejo@sympower.net>
2025-03-03 23:21:42 +0100

    remove duplicated repositories

--- client/src/components/RepositoryList.tsx
+++ client/src/components/RepositoryList.tsx
@@ -1,11 +1,14 @@
 import { subscribeKeys } from "nanostores";
 import { useEffect, useState } from "react";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, type InfiniteData } from "@tanstack/react-query";
 
 import { $repositoriesParamsStore, initialState } from "../stores/repositories";
 import { getGithubRepositories } from "../requests/repositories";
 import styles from "./RepositoryList.module.css";
-import type { GetGithubRepositoriesParams } from "../models/repositories";
+import type {
+  GetGithubRepositoriesParams,
+  GetGithubRepositoriesResponse,
+} from "../models/repositories";
 import { queryClient } from "../stores/queryClient";
 import RepositoryCard from "./RepositoryCard";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -13,25 +16,58 @@ import InfiniteScroll from "react-infinite-scroll-component";
 function RepositoryList() {
   const [repositoriesParams, setRepositoriesParams] =
     useState<GetGithubRepositoriesParams>(initialState);
-  const { data, isPending, fetchNextPage } = useInfiniteQuery(
-    {
-      initialPageParam: 1,
-      queryKey: ["GET_REPOSITORY_LIST", repositoriesParams],
-      queryFn: ({ pageParam }) => {
-        return getGithubRepositories({
-          language: repositoriesParams.filters.language,
-          page: pageParam,
-          perPage: repositoriesParams.pagination.perPage,
-        });
-      },
-      getNextPageParam(_, pages) {
-        const pagesLength = pages.length;
+  const { data, isPending, isFetching, hasNextPage, fetchNextPage } =
+    useInfiniteQuery(
+      {
+        initialPageParam: 1,
+        queryKey: ["GET_REPOSITORY_LIST", repositoriesParams],
+        queryFn: async ({ pageParam, signal, client }) => {
+          // As Github can return duplicate items on different pages, we have to clean them before showing them.
+          // In order to clean up them, we compare the latest result from the API with the one from the
+          // cache, and remove the duplications.
+          const result = await getGithubRepositories(
+            {
+              language: repositoriesParams.filters.language,
+              page: pageParam,
+              perPage: repositoriesParams.pagination.perPage,
+            },
+            { signal },
+          );
+          const cacheData = client.getQueryData<
+            InfiniteData<GetGithubRepositoriesResponse>
+          >(["GET_REPOSITORY_LIST", repositoriesParams]);
 
-        return pagesLength + 1;
+          if (!cacheData) {
+            return result;
+          }
+
+          const { pages } = cacheData;
+          const pagesLength = pages.length;
+          const lastPage = pages[pagesLength - 1];
+          const lastPageRepositoryIds = new Set(
+            lastPage.repositories.map((repository) => repository.id),
+          );
+
+          return {
+            totalCount: result.totalCount,
+            repositories: result.repositories.filter(
+              (repository) => !lastPageRepositoryIds.has(repository.id),
+            ),
+          };
+        },
+        getNextPageParam(_, pages) {
+          const pagesLength = pages.length;
+
+          return pagesLength + 1;
+        },
+        getPreviousPageParam(_, pages) {
+          const pagesLength = pages.length;
+
+          return pagesLength - 1;
+        },
       },
-    },
-    queryClient,
-  );
+      queryClient,
+    );
 
   const repositories = data?.pages.flatMap((page) => page.repositories) ?? [];
   const totalCount = data?.pages[0].totalCount ?? 0;
@@ -61,7 +97,7 @@ function RepositoryList() {
       <InfiniteScroll
         dataLength={repositories.length ?? 0}
         next={fetchNextPage}
-        hasMore={repositories.length < totalCount}
+        hasMore={!isFetching && hasNextPage && repositories.length < totalCount}
         scrollThreshold={0.7}
         loader={<h4>Loading...</h4>}
         className={styles.infiniteScrollContainer}
