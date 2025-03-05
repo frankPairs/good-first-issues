f0d32b66bcc2a7c1169674af88495594c3e90fca
Frank Parejo <frank.parejo@sympower.net>
2025-03-05 22:34:18 +0100

    navigate to github good first issues when clicking repository card

--- client/src/components/RepositoryCard.tsx
+++ client/src/components/RepositoryCard.tsx
@@ -7,43 +7,55 @@ type Props = {
   repository: GithubRepository;
 };
 
+function generateRepositoryIssuesUrl(repositoryUrl: string): string {
+  const url = new URL(`${repositoryUrl}/issues`);
+
+  url.searchParams.append("q", 'is:issue state:open label:"good first issue"');
+
+  return url.toString();
+}
+
 function RepositoryCard({ repository }: Readonly<Props>) {
   return (
-    <article className={styles.container}>
-      <header className={styles.header}>
-        <div className={styles.headerContent}>
-          <img
-            className={styles.headerAvatar}
-            src={repository.avatarUrl}
-            alt={repository.name}
-          />
+    <a
+      href={generateRepositoryIssuesUrl(repository.url)}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <article className={styles.container}>
+        <header className={styles.header}>
+          <div className={styles.headerContent}>
+            <img
+              className={styles.headerAvatar}
+              src={repository.avatarUrl}
+              alt={repository.name}
+            />
 
-          <a href={repository.url} target="_blank" rel="noopener noreferrer">
             <h2 className={styles.headerTitle}>{repository.name}</h2>
-          </a>
-        </div>
-      </header>
-
-      <div className={styles.content}>
-        <p className={styles.contentDescription}>{repository.description}</p>
-      </div>
-
-      <footer className={styles.footer}>
-        <div className={styles.footerContent}>
-          <div className={styles.footerInfoContainer}>
-            <StarIcon className={styles.footerIcon} />
-
-            <p className={styles.footerText}>{repository.starsCount}</p>
           </div>
+        </header>
 
-          <div className={styles.footerInfoContainer}>
-            <IssuesIcon className={styles.footerIcon} />
-
-            <p className={styles.footerText}>{repository.openIssuesCount}</p>
-          </div>
+        <div className={styles.content}>
+          <p className={styles.contentDescription}>{repository.description}</p>
         </div>
-      </footer>
-    </article>
+
+        <footer className={styles.footer}>
+          <div className={styles.footerContent}>
+            <div className={styles.footerInfoContainer}>
+              <StarIcon className={styles.footerIcon} />
+
+              <p className={styles.footerText}>{repository.starsCount}</p>
+            </div>
+
+            <div className={styles.footerInfoContainer}>
+              <IssuesIcon className={styles.footerIcon} />
+
+              <p className={styles.footerText}>{repository.openIssuesCount}</p>
+            </div>
+          </div>
+        </footer>
+      </article>
+    </a>
   );
 }
 
